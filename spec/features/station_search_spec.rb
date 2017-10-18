ae3d8a5a25dc4506d839e2bd373db970d3281c50
Jim Szalewski <jim.szalewski@gmail.com>
2017-10-18 09:34:49 -0600

    search feature test written, search index view is built

---
+++ spec/features/station_search_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'A user visits the root path' do
+  context 'enters parameters and clicks Locate' do
+    it 'and sees a list of stations' do
+
+      visit '/'
+
+      fill_in 'q', with: '80203'
+
+      click_on 'Locate'
+
+      expect(current_path).to eq search_path
+      expect(page).to have_content('10 Stations within 6 miles')
+      expect(page).to have_css('.station', count: 10)
+
+      within(first('.station')) do
+        expect(page).to have_css('.name')
+        expect(page).to have_css('.address')
+        expect(page).to have_css('.fuel_types')
+        expect(page).to have_css('.distance')
+        expect(page).to have_css('.access_times')
+      end
+      # And the stations should be limited to Electric and Propane
+
+    end
+  end
+end
