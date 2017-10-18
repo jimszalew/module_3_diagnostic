be2eaf02824977f7ffe250f326da48289c98eaee
Jim Szalewski <jim.szalewski@gmail.com>
2017-10-18 11:25:15 -0600

    if you were fortunate enough to get nrel to give you an api key, you were far and away better off than everyone else, incredibly lop sided

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,11 @@
 class SearchController < ApplicationController
   def index
+    search = params[:q]
     @stations = NrelService.new(search)
 
 
 
 
-    search = params[:q]
 
     response = Faraday.get "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_api_key']}&fuel_type=LPG,ELEC&location=#{search}&radius=6&limit=10"
 
