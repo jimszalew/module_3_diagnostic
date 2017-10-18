a8ba74f7e38888403b475ba355d2b367f2d02d62
Jim Szalewski <jim.szalewski@gmail.com>
2017-10-18 10:05:14 -0600

    attempting to setup api call in controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,9 @@
 class SearchController < ApplicationController
   def index
-    
+    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=#{ENV['nrel_api_key']}") do |faraday|
+      faraday.headers['fuel_type'] = 'LPG,ELEC'
+      faraday.adapter Faraday.default_adapter
+      require "pry"; binding.pry
+    end
   end
 end
