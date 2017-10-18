467e700a49a4992a9e3268d33115a4974d58bd00
Jim Szalewski <jim.szalewski@gmail.com>
2017-10-18 10:20:20 -0600

    api call working in controller, parsing station information

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,9 @@
 class SearchController < ApplicationController
   def index
-    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=#{ENV['nrel_api_key']}") do |faraday|
-      faraday.headers['fuel_type'] = 'LPG,ELEC'
-      faraday.adapter Faraday.default_adapter
+    response = Faraday.get "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=#{ENV['nrel_api_key']}&fuel_type=LPG,ELEC"
+
+    @stations = JSON.parse(response.body, symbolize_names: true)
+
       require "pry"; binding.pry
-    end
   end
 end
